44bc9650c3c50cbf1de3e4f2307332a3790b75ff
olena-nikitchenko <113857063+olena-nikitchenko@users.noreply.github.com>
2023-10-15 22:28:56 +0300

    Update 2.ts

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,7 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare (top: AllType, bottom: AllType): AllType {
   return {
     name: top.name,
     color: top.color,
@@ -19,5 +19,7 @@ function compare (top, bottom): AllType {
     weight: bottom.weight,
   }
 }
+
+
 
 export {};
