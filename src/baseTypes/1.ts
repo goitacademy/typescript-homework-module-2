3c2770a9f70f1f953b5f1d547967174ccf9a9b1e
olena-nikitchenko <113857063+olena-nikitchenko@users.noreply.github.com>
2023-10-15 18:16:02 +0300

    Update 1.ts

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,10 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let age:number = 50;
+let name:string = 'Max';
+let toggle:boolean = true;
+let empty:null = null;
+let notInitialize: unknown;
+let callback = (a:number): number => { return 100 + a };
 
 export {};
