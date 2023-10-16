edfb491208d555829b884d464c2cc5ba2dac55c9
olena-nikitchenko <113857063+olena-nikitchenko@users.noreply.github.com>
2023-10-16 21:20:27 +0300

    Update 1.ts

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -4,7 +4,7 @@ let age:number = 50;
 let name:string = 'Max';
 let toggle:boolean = true;
 let empty:null = null;
-let notInitialize: unknown;
+let notInitialize: undefined;
 let callback = (a:number): number => { return 100 + a };
 
 export {};
