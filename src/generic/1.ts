8137ef858422f26d54cca6384192a9945d8273f7
olena-nikitchenko <113857063+olena-nikitchenko@users.noreply.github.com>
2023-10-16 22:01:20 +0300

    Update 1.ts

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,13 +3,13 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise <P>(arg:P): Promise<P> {
+function getPromise (): Promise<(string | number)[]> {
   return new Promise((resolve) => {
-    resolve(arg);
+    resolve(['Text', 50]);
   });
 }
 
-getPromise(['Text', 50])
+getPromise()
 .then((data) => {
   console.log(data);
 });
