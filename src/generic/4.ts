9f8f95250688c611bfbd30776967c8f1614bde8e
olena-nikitchenko <113857063+olena-nikitchenko@users.noreply.github.com>
2023-10-16 18:51:13 +0300

    Update 4.ts

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface T<TitleType> {
+  title: TitleType;
+}
 
-class Component {
-  constructor (public props:T) {
+class Component<PropsType> {
+  constructor (public props: T<PropsType>) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<string> {
   pageInfo () {
     console.log(this.props.title);
   }
